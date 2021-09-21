5251b05c568f6299f10f7b410e266e06e559f2f5
StevenD <dorvilsteven@gmail.com>
2021-09-20 23:05:55 -0400

    display on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
-  $(document).ready(function(){
-    $('select').formSelect();
-  });
+var sKEY = 'dbefcc9a25f8481a9786d8ffcf0b2c2e';
+var recipeContent = $("#recipe-content");
+
+// display recipe on screen 
+// missing instructions
+var getRecipe = function(title, image, main, other) {
+  // clear html for recipe content 
+  recipeContent.html("");
+  // create elememts
+  var recipeTitle = $("<h3>").text(title);
+  var recipeImg = $("<img>").attr('src', image);
+  var divMain = $("<div>").addClass('main-ingredients').append($("<ul>").addClass('collection'));
+  var divOther = $("<div>").addClass('other-ingredients').append($("<ul>").addClass('collection'));
+  console.log(other);
+  // loop to pupolate main and other Ingredients
+  for (var i=0;i<main.length;i++) {
+    divMain.children('.collection').append($("<li>").addClass("collection-item").text(main[i].original));
+  }
+  for (var i=0;i<other.length;i++) {
+    divOther.children('.collection').append($("<li>").addClass("collection-item").text(other[i].original));
+  }
+  // append elements
+  recipeContent.append(recipeTitle, recipeImg, divMain, divOther);
+
+};
+
 
 $('#search-button').on('click', function() {
     // gets user ingredients from search bar 
@@ -15,12 +38,31 @@ $('#search-button').on('click', function() {
         finished +=  `+${userIngredients[i]},`  
     }
 
-    fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${finished}&number=100&apiKey=dbefcc9a25f8481a9786d8ffcf0b2c2e`).then(function(response) {
+    fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${finished}&number=100&apiKey=${sKEY}`).then(function(response){
         return response.json();
     }).then(function(data) {
-    // Have user select desired recipe
-    //Store users data in a variable by getting the id of that recipe
-    console.log(data);
-    //second fetch that would lead to the actual recipe that the user wants
+      console.log(data);
+      // get random number to get random element in data array 
+      var randomNum = Math.floor(Math.random() * data.length);
+      var food = data[randomNum];
+
+      // main data 
+      var id = data[randomNum].id;
+      var title = data[randomNum].title;
+      var image = data[randomNum].image;
+      var mainIngredients = data[randomNum].usedIngredients;
+      var otherIngredients = data[randomNum].missedIngredients;
+  
+      // ask TA 
+      // instructions
+      // fetch(`https://api.spoonacular.com/recipes/?id=${id}/analyzedInstructions&apiKey=${sKEY}`).then(function(response) {
+      //   return response.text();
+      // }).then(function(data) {
+      //   console.log(data);
+      // });
+      
+      // display on screen
+      getRecipe(title, image, mainIngredients, otherIngredients);
+
     })
 });
